ff955871053c8215aa3418b795f5888b772cc567
Sam McCaffrey <smccaffrey70@gmail.com>
2024-11-03 12:10:57 -0700

    adding make lint command

--- llm/api/deps/ai.py
+++ llm/api/deps/ai.py
@@ -5,7 +5,7 @@ from transformers.pipelines.base import Pipeline  # type: ignore
 MODEL_NAME = "gpt2"
 
 
-@lru_cache()
+@lru_cache()  # caches model not data
 def get_model() -> Pipeline:
     generator: Pipeline = pipeline(task="text-generation", model=MODEL_NAME)
     return generator
